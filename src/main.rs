3935f2c5f5eee71549daf6331003ba1bd95582c3
masato-210831 <b86500b@gmail.com>
2024-06-14 15:43:47 +0000

    rust 関数

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,23 @@
-use std::ops::Index;
 
+fn say_hello() {
+    println!("Hello!");
+}
+
+fn add(a: i32, b:i32) -> i32 {
+    a + b
+}
 
 fn main() {
-   let v1 = vec![1, 2, 3];
 
-   let mut v3 = Vec::new();
-   v3.push(1);
-   v3.push(2);
-   v3.push(3);
 
-   println!("{:?}", v3);
+   say_hello();
+   println!("{}", add(1, 3));
+   let c = add(1, 3);
+   let d = say_hello();
 
-   let x = v3.pop();
 
-   println!("{:?}", x);
-   println!("{:?}", v3);
 
-   let y = v3[1];
-   let z = v3.get( 100);
-   println!("{:?}", z);
 
-   let s = &v3[..1];
 
 }   
 
